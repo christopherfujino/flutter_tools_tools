29f7037d1100e556516e5f4cb8ca11d983606cfc
Christopher Fujino <christopherfujino@gmail.com>
2022-09-26 15:04:05 -0700

    wip

---
+++ chrome_extension/lib/go_crash.js
@@ -0,0 +1,1 @@
+console.log("Gonna sort these crashes!");
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -5,25 +5,19 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+const goCrashButton = document.getElementById("go-crash");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+// When the button is clicked, inject func into current page
+goCrashButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   console.log(tab);
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: setPageBackgroundColor,
+    func: goCrash,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function goCrash() {
+  console.log("Sort the crashes!");
 }
