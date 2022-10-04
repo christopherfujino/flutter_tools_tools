2dc703389626de0fa08a968d098ac91cf8eb2a50
Christopher Fujino <christopherfujino@gmail.com>
2022-10-04 10:59:34 -0700

    link to system libgit2 (statically linking would still have runtime deps)

--- git_to_json/c/main.c
+++ git_to_json/c/main.c
@@ -1,17 +1,19 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "git2.h"
 
 void init();
-void clone(git_repository **repo, const char *upstream_url,
-           const char *local_path);
+bool clone(git_repository **repo, const char *upstream_url,
+           const char *local_path, bool allow_failure);
 void open_repo(git_repository **repo, const char *local_path);
-void status(git_repository *repo);
+void name_each_ref(git_repository *repo);
 void deinit();
 
-void check(int result);
+void check(int result, const char *description);
 
 const char *local_repo_path = "./ephemeral_state/dotfiles";
+const int MESSAGE_MAX = 200;
 
 // struct repository {};
 
@@ -21,10 +23,11 @@ int main() {
   init();
 
   git_repository *repo = NULL;
-  clone(&repo, "http://github.com/christopherfujino/dotfiles",
-        "./ephemeral_state/dotfiles");
+  clone(&repo, "http://github.com/flutter/flutter",
+        "./ephemeral_state/dotfiles", true);
+  git_repository_free(repo);
   open_repo(&repo, local_repo_path);
-  status(repo);
+  name_each_ref(repo);
   deinit();
   return 0;
 }
@@ -35,40 +38,46 @@ void init() {
 }
 
 void deinit() { git_libgit2_shutdown(); }
-void clone(git_repository **repo, const char *upstream_url,
-           const char *local_path) {
-  check(git_clone(repo, // TODO should be freed
-                  upstream_url, "./ephemeral_state/dotfiles",
-                  NULL // const git_clone_options *options
-                  ));
+bool clone(git_repository **repo, const char *upstream_url,
+           const char *local_path, bool allow_failure) {
+  int result = git_clone(repo, // TODO should be freed
+                         upstream_url, "./ephemeral_state/dotfiles",
+                         NULL // const git_clone_options *options
+  );
+
+  if (!allow_failure) {
+    char message[MESSAGE_MAX];
+    snprintf(message, 200, "git clone failed to clone %s to %s", upstream_url, local_path);
+    check(result, message);
+    return true;
+  }
+  return result == 0;
 }
 
 /// Open a repo from disk.
 void open_repo(git_repository **repo, const char *local_path) {
-  check(git_repository_open(repo, local_path)); // TODO repo should be freed
+  char message[MESSAGE_MAX];
+  snprintf(message, MESSAGE_MAX, "Failed to load git repo from %s", local_path);
+  check(git_repository_open(repo, local_path), message);
 }
 
-typedef struct {
-  int foo;
-} status_data;
+void name_each_ref(git_repository *repo) {
+  // https://stackoverflow.com/questions/7064314/what-is-0-in-c
+  git_strarray refs = {0};
+  check(git_reference_list(&refs, repo), "naming refs");
 
-int status_callback(const char *path, unsigned int status_flags,
-                    void *payload) {
-  status_data *ptr = (status_data *)payload;
-  printf("wow! %d\n", ptr->foo);
-  return 0;
-}
+  for (int i = 0; i < refs.count; i++) {
+    printf("Ref: \"%s\"\n", refs.strings[i]);
+  }
 
-void status(git_repository *repo) {
-  status_data payload = {.foo = 0};
-  check(git_status_foreach(repo, status_callback, &payload));
+  git_strarray_free(&refs);
 }
 
 /// Log error and exit 1 if result is not 0.
-void check(int result) {
+void check(int result, const char *description) {
   if (result != 0) {
     const git_error *error = git_error_last();
-    fprintf(stderr, "Error %d/%d: %s\n", result, error->klass, error->message);
+    fprintf(stderr, "Error %s %d/%d: %s\n", description, result, error->klass, error->message);
     exit(1);
   }
 }
