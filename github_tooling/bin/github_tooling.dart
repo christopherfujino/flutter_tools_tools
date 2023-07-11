5b3dfe71698d045d568d581a9eefcd3afc599543
Christopher Fujino <christopherfujino@gmail.com>
2023-07-11 15:46:46 -0700

    init github_tooling

---
+++ github_tooling/bin/github_tooling.dart
@@ -0,0 +1,5 @@
+import 'package:github_tooling/github_tooling.dart' as github_tooling;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_tooling.calculate()}!');
+}
---
+++ github_tooling/test/github_tooling_test.dart
@@ -0,0 +1,8 @@
+import 'package:github_tooling/github_tooling.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
